4849abbe33b3a727606f5e2cc2183c476c84538a
GretaRa <ggretute88@gmail.com>
2022-07-27 16:06:58 +0200

    Create one game round for user vs computer

--- JS/javascript.js
+++ JS/javascript.js
@@ -12,22 +12,58 @@ function getComputerChoice(){
     return(ComputerChoice)
 }
 
-//play single round of game
-
-function playRound(){
-    //get user input
+//get user input
     let playerSelection = (prompt('Please choose rock, paper or scissors:')).toLowerCase();
-    //create variable to be stable to have stable value (like for console.log)
+    console.log(playerSelection)
+//create variable to have stable value (for console.log)
     let computerSelection = getComputerChoice();
     console.log(computerSelection);
+
+//play single round of game
+
+function playRound(playerSelection, computerSelection){
+   
     //make conditions
     if (playerSelection === computerSelection){
-        alert('It\'s a tie!');
-    } else if {
-        
+        console.log('It\'s a tie!');
+        return null;
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+        console.log('You Win! Rock beats Scissors');
+        return true;
+    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        console.log('You Win! Paper beats Rock');
+        return true;
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+        console.log('You Win! Scissors beat rock');
+        return true;
+    } else if (playerSelection === 'rock' && computerSelection === 'paper'){
+        console.log('You Lose! Paper beats Rock');
+        return false;
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+        console.log('You Lose! Scissors beat Paper');
+        return false;
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+        console.log('You Lose! Rock beats Scissors');
+        return false;
+    } else {
+        console.log('Wrong input!');
+        return;
     }
 }
-playRound()
+
+
+function game(){
+    for (let i = 0; i < 5; i++) {
+        let gameResult = playRound();
+        console.log(gameResult)
+     }
+    
+
+    //  let overallResult = Array(gameResult);
+    //  console.log(overallResult)
+}
+game()
+
 
 //get input form user, make it case-insensitive
 
