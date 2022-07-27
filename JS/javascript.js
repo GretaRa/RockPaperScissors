2d810bf3b34828f7d4580ac5f76634e7d3ddce01
GretaRa <ggretute88@gmail.com>
2022-07-27 17:37:31 +0200

    Create a console game of 5 rounds

--- JS/javascript.js
+++ JS/javascript.js
@@ -9,15 +9,15 @@ function getComputerChoice(){
     //take random value from the array
     let ComputerChoice = possible_choices[Math.floor(Math.random()* possible_choices.length)];
 
-    return(ComputerChoice)
+    return(ComputerChoice);
 }
 
-//get user input
-    let playerSelection = (prompt('Please choose rock, paper or scissors:')).toLowerCase();
-    console.log(playerSelection)
-//create variable to have stable value (for console.log)
-    let computerSelection = getComputerChoice();
-    console.log(computerSelection);
+// //get user input
+//     let playerSelection = (prompt('Please choose rock, paper or scissors:')).toLowerCase();
+//     console.log(playerSelection)
+// //create variable to have stable value (for console.log)
+//     let computerSelection = getComputerChoice();
+//     console.log(computerSelection);
 
 //play single round of game
 
@@ -52,17 +52,43 @@ function playRound(playerSelection, computerSelection){
 }
 
 
+
 function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+
     for (let i = 0; i < 5; i++) {
-        let gameResult = playRound();
-        console.log(gameResult)
+        let outcome;
+        while (outcome === undefined){
+            //get user input
+            let playerSelection = (prompt('Please choose rock, paper or scissors:')).toLowerCase();
+            console.log(playerSelection);
+
+            let computerSelection = getComputerChoice();
+            console.log(computerSelection);
+            
+            outcome = playRound(playerSelection, computerSelection);
+            console.log(outcome);
+        }
+
+        if (outcome === true){
+            playerScore++ ;
+            console.log(playerScore);
+        } else if(outcome === false){
+            computerScore++ ;
+            console.log(computerScore);
+        }
      }
     
-
-    //  let overallResult = Array(gameResult);
-    //  console.log(overallResult)
+    if (playerScore === computerScore){
+        console.log('It\'s a tie');
+    } else if (playerScore > computerScore){
+        console.log('You win!');
+    } else {
+        console.log('You lose :(');
+    }
 }
-game()
+game();
 
 
 //get input form user, make it case-insensitive
